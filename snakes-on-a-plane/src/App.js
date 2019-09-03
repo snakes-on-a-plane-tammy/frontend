d1f56b091b16ad32fa43c009981319be717e46a2
tammytdo11 <tammytdo11@gmail.com>
2019-09-03 01:38:51 -0700

    very simple start. Plane.js added

---
+++ snakes-on-a-plane/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Plane from './Plane.js'
+
+function App() {
+  return (
+    <div>
+      <h1>Plane soon!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ snakes-on-a-plane/src/Plane.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Plane extends React.Component {
+    render() {
+        return <h1>Plane pronto</h1>
+    }
+}
+
+expirt default Plane
